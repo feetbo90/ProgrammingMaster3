15754050ac9554932e851a7a29621a5dd09658b1
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-06-13 17:41:01 +0700

    pengulangan

---
+++ src/ListInKotlin.kt
@@ -0,0 +1,44 @@
+
+/*
+
+    List<Int> 5
+
+    0   1   2   3   4
+    10  30            40
+
+
+    100 140 150 190
+    100 140 50 150 190
+
+ */
+
+
+fun main(args: Array<String>) {
+
+    val list = mutableListOf<Int>()
+
+    list.add(100)
+    list.add(121)
+    list.add(130)
+    list.add(3, 200)
+
+    println(list[3])
+    list.add(2,150)
+    println(list)
+
+    list.removeAt(0)
+    println(list)
+
+    val input = readLine()!!.toInt()
+    list.add(input)
+    list[0] = 123
+
+    for (i in 0 until list.size) {
+
+        if (list[i] % 2 == 1)
+            println(list[i])
+    }
+
+    println(list)
+
+}
