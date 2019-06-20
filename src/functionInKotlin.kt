2483bf9be7ff3e0d6b7f5de59af337567338bcc9
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-06-20 20:08:56 +0700

    tipe fungsi baru

--- src/functionInKotlin.kt
+++ src/functionInKotlin.kt
@@ -18,8 +18,40 @@ fun main(args: Array<String>) {
     penjumlahan(bilangan, bilangan2)
     //val kurang = objek.pengurangan(bilangan, bilangan2)
     val kurang = pengurangan(bilangan, bilangan2)
-    print("Maka pengurangan : $kurang")
+    println("Maka pengurangan : $kurang")
+
+    val kali = bilangan.kali(bilangan2)
+    println(kali)
+    // Int.kali(Int)
+    val bagi = 4 dibagi 2
+
+    val list = mutableListOf<Int>()
+    list.add(3)
+    list.add(4)
+    tampilkanData(list)
 }
+
+// fungsi dengan parameter array
+fun tampilkanData(list: List<Int>) {
+
+    for (i in 0 until list.size) {
+        println("maka nilai $i : ${list[i]}")
+    }
+}
+
+// fungsi extension
+fun Int.kali(bilangan2: Int) : Int{
+
+    return this * bilangan2
+}
+
+// fungsi infix
+infix fun Int.dibagi(bilangan2: Int): Int{
+
+    return this / bilangan2
+}
+
+
 
 // fungsi tanpa pengembalian nilai
 fun penjumlahan(bilangan : Int, bilangan2: Int) {
@@ -32,3 +64,6 @@ fun pengurangan(bilangan: Int, bilangan2: Int) : Int {
     val kurang = bilangan - bilangan2
     return kurang
 }
+
+
+
