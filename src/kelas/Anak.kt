70411ff43104e21d4ae5a418fc7ecd16a66e7402
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-07-19 11:10:33 +0700

    Tugas

--- src/kelas/Anak.kt
+++ src/kelas/Anak.kt
@@ -23,3 +23,37 @@ fun main(args: Array<String>) {
     val kevin = Anak(1,2,2)
     kevin.baikHati()
 }
+
+/*
+
+    class dosen : nama, nidn, kelas yang diajarkan
+    list nama, list nidn, list kelas
+    list nama#nidn#kelas
+    fungsi tambah nama dosen : nama, nidn, kelas
+    fungsi cariDosen("Saib") {
+
+        return SaibSuwilo, 2344, KDM
+    }
+
+
+    class mahasiswa : nama, nim, nama dosen
+
+    nama : Saib Suwilo
+    nidn : 10009877333
+    kelas : KDM
+
+    objek kevin, 19039393, Saib Suwilo
+
+    kevin = Mahasiswa()
+
+    cariDosen("Saib Suwilo")
+
+
+    Masukkan data dosen
+    1
+
+    Masukkan Data mahasiswa
+    1
+
+    cari Dosen : Saib Suwilo
+ */
